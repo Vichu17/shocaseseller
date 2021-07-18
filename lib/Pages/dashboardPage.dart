402642ec71c1784a0af839e96524af2d2fe4f197
Vishnu CK <vishnu.ckv@gmail.com>
2021-07-18 21:29:19 +0530

    SignIn, LogIn, Dashboard Completed

---
+++ lib/Pages/dashboardPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DashboardPageSeller extends StatefulWidget {
+  const DashboardPageSeller({Key? key}) : super(key: key);
+
+  @override
+  _DashboardPageSellerState createState() => _DashboardPageSellerState();
+}
+
+class _DashboardPageSellerState extends State<DashboardPageSeller> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Pages/loginPage.dart
@@ -0,0 +1,175 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shocaseseller/Pages/dashboardPage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _auth = FirebaseAuth.instance;
+  late User loggedUser;
+  late String email;
+  late String password;
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 500),
+            decoration: BoxDecoration(
+              color: Color(0xFF0e2149),
+            ),
+            child: Image.asset(
+              'assets/logo.png',
+              height: 306,
+              width: 306,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 350),
+            child: Center(
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 6,
+                    fontWeight: FontWeight.w300,
+                  ),
+                )),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 250.0, left: 10.0, right: 10.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Container(
+                height: 300,
+                width: 360,
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(top: 10.0),
+                          child: Text(
+                            'Please enter your Personal Info',
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Container(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              email = value;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              hintText: 'Email',
+                              hintStyle: TextStyle(color: Colors.grey[400]),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 20.0),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Container(
+                          width: 300,
+                          child: TextField(
+                            onChanged: (value) {
+                              password = value;
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              hintText: 'Password',
+                              hintStyle: TextStyle(color: Colors.grey[400]),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 20.0),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 1.0),
+                          child: Material(
+                            color: Color(0xFF0e2149),
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(10.0)),
+                            child: MaterialButton(
+                              onPressed: () async {
+                                try {
+                                  final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                      email: email, password: password);
+                                  if (user != null) {
+                                    Navigator.pushNamed(context, '/dashboard');
+                                  }
+                                } catch (e) {
+                                  Fluttertoast.showToast(
+                                      msg:
+                                      "Please check you Email and Password",
+                                      toastLength: Toast.LENGTH_LONG,
+                                      gravity: ToastGravity.CENTER,
+                                      backgroundColor: Colors.white,
+                                      textColor: Colors.black);
+                                }
+                              },
+                              minWidth: 300.0,
+                              height: 20.0,
+                              child: Text(
+                                'SignIn',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
